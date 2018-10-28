8162901241abd32d5d02d32c8d615f3857838104
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2018-10-27 17:26:41 -0700

    Add documentation

--- R/segmentedReg-package.R
+++ R/segmentedReg-package.R
@@ -1,4 +1,4 @@
 #' @keywords internal
 "_PACKAGE"
 
-This package allows to run a segmented (also known as threshold regression).
+#' This package allows to run a segmented (also known as threshold regression).
