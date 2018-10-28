d7e6c9faf3fcfcd2899fcdee405f5a42ecbe7c5d
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2018-10-27 19:52:56 -0700

    export sim dat

--- R/sim_dat.R
+++ R/sim_dat.R
@@ -5,8 +5,9 @@
 #' @return A data frame with columns *y*, *x1* and *x2*
 #' @examples
 #' data_thresh <- sim_thresh()
+#' @export
 
-
+#' @importFrom stats rnorm
 sim_thresh <-  function(N = 200, th_val =0) {
   x1 <- rnorm(N)
   # x2 <- rnorm(N)
