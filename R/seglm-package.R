fa5eac13df43b1808a2cfabfc2f3a666b7295690
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-02-23 10:34:14 -0800

    adapt general package documentation

--- R/seglm-package.R
+++ R/seglm-package.R
@@ -1,3 +1,10 @@
+#' seglm: a package for segmented regression, also known as threshold regression, piecewise regression, or regression with structural break
+#'
+#' The main function is seglm
+#' @name seglm-package
+#' @docType package
+NULL
+
 #' @keywords internal
 "_PACKAGE"
 #' @importFrom stats lm,lm.fit
